3d837b77b356858bdb01bd874415fb0ab17556c8
Heisenberg <q55915721@gmail.com>
2019-01-29 21:26:06 +0800

    Initial Commit

---
+++ Stretchy Header Collection View Cells/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stretchy Header Collection View Cells
+//
+//  Created by 洪森達 on 2019/1/29.
+//  Copyright © 2019 洪森達. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
